b0db9c39cef19d6097c62eb4124a0ed37b965129
Ian C <9bitStudios@gmail.com>
2014-04-07 08:21:12 -0700

    Adding updated files...

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-
 var APPLICATION_ROOT = __dirname;
 var PORT = 1337;
 
-var http = require('http');
-var tw = require('tw');
+var express = require('express'); 
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
-http.createServer(function (request, response) {
-	
-	tw.route(request, response);
-	
-}).listen(PORT);
+// Server
+var server = express();
+
+//Static content 
+server.use('/app', express.static(APPLICATION_ROOT + '/app'));
+
+//Show all errors in development
+server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+// Database
+var database = require('./database')(express, server, mongoose);
+
+// Routes
+require('./api')(express, server, mongoose, database);
+
+// Start server 
+
+server.listen(PORT, '0.0.0.0', function() {
+    console.log( 'Express server listening on port %d in %s mode', PORT, server.settings.env );
+});
  
