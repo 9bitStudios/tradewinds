c7e6e0599104cf91bb494d18923f0ec9f29b0cc8
Ian C <9bitStudios@gmail.com>
2014-04-10 08:24:13 -0700

    Moving database module to routes directory... Adding additional dependencies... Removing api.js...

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var db = require('./database');
+var mongoose = require('mongoose');
+
 exports.index = function(request, response){
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
