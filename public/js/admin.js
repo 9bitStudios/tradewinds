0ab40e513731f4480364afdc483d584113f9e672
Ian C <9bitStudios@gmail.com>
2014-05-19 05:06:20 -0700

    Adding additional features to content editor... Adding link to view posts in admin panel...

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,10 @@
 tinymce.init({
-    selector: "textarea.tw-editor"
+    selector: "textarea.tw-editor",
+    plugins: [
+	"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+	"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+	"save table contextmenu directionality emoticons template paste textcolor"
+   ],    
  });
 
 jQuery(document).ready(function(){
