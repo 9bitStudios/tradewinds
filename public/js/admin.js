70ad4a30fd7dafa81ff7c48f06ecbd6c5a6766cb
Ian C <9bitStudios@gmail.com>
2014-06-12 07:30:50 -0700

    Adding path/URL information for posts in admin panel... Updating all categories view to exclude default...

--- public/js/admin.js
+++ public/js/admin.js
@@ -12,7 +12,7 @@ tinymce.init({
 TradewindsAdmin = (function () {
  
     ConvertToSlug = function(text) {
-	return text.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+	return text.trim().toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
     };
  
     return {
@@ -23,12 +23,28 @@ TradewindsAdmin = (function () {
 
 jQuery(document).ready(function(){
     jQuery(".tw-admin-post-datepicker").datepicker({dateFormat : "yy/mm/dd"});
-	
+
+    function updatePath(){
+	var category = jQuery('.tw-admin-post-category option:selected').attr('data-slug').toLowerCase();
+	if(category != "") {
+	    category = "/" + category;
+	}
+	jQuery('.tw-admin-post-path').text(window.location.protocol + '//' + window.location.host + category + '/' + jQuery('.tw-admin-post-slug').val());    
+    }      
+    
     jQuery('.tw-admin-convert-slug').click(function(e) {
 	e.preventDefault();
 	var text = TradewindsAdmin.ConvertToSlug(jQuery('.tw-admin-post-title').val());
 	jQuery('.tw-admin-post-slug').val(text);
-	
+	updatePath();
     });
-	
+      
+    updatePath();
+    
+    jQuery('.tw-admin-post-category, .tw-admin-post-slug').change(function(){
+	updatePath();
+    });
+    
+
+    
 });
