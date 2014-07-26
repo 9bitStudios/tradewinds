b8b35c686e8eab6a2c662bf817692faed5dca6e0
Ian C <9bitStudios@gmail.com>
2014-07-26 09:42:46 -0700

    Moving admin JavaScript to individual pages to avoid errors involving function calls on non-existent selectors... Adding in initial menu creation prototype...

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,14 +1,5 @@
 var TradewindsAdmin = TradewindsAdmin || { }; 
 
-tinymce.init({
-    selector: "textarea.tw-admin-editor",
-    plugins: [
-	"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-	"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-	"save table contextmenu directionality emoticons template paste textcolor"
-   ],    
- });
-
 TradewindsAdmin = (function () {
  
     ConvertToSlug = function(text) {
@@ -21,30 +12,3 @@ TradewindsAdmin = (function () {
      
 })();
 
-jQuery(document).ready(function(){
-    jQuery(".tw-admin-post-datepicker").datepicker({dateFormat : "yy/mm/dd"});
-
-    function updatePath(){
-	var category = jQuery('.tw-admin-post-category option:selected').attr('data-slug').toLowerCase();
-	if(category != "") {
-	    category = "/" + category;
-	}
-	jQuery('.tw-admin-post-path').text(window.location.protocol + '//' + window.location.host + category + '/' + jQuery('.tw-admin-post-slug').val());    
-    }      
-    
-    jQuery('.tw-admin-convert-slug').click(function(e) {
-	e.preventDefault();
-	var text = TradewindsAdmin.ConvertToSlug(jQuery('.tw-admin-post-title').val());
-	jQuery('.tw-admin-post-slug').val(text);
-	updatePath();
-    });
-      
-    updatePath();
-    
-    jQuery('.tw-admin-post-category, .tw-admin-post-slug').change(function(){
-	updatePath();
-    });
-    
-
-    
-});
