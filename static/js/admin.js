a86acafc9b2eb4530c030ba9b40ef195f436e01e
unknown <9bitStudios@gmail.com>
2014-11-30 07:04:09 -0800

    Utility functions for admin.js...

--- static/js/admin.js
+++ static/js/admin.js
@@ -2,6 +2,89 @@ var Tradewinds = Tradewinds || { };
 
 Tradewinds.Admin = {
  
+    Utilities: {
+        
+        CookieHandler: {
+            
+            createCookie: function (name, value, days) {
+
+               var expires;
+               if (days) {
+                   var date = new Date();
+                   date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                   expires = "; expires=" + date.toGMTString();
+               } else {
+                   expires = "";
+               }
+               document.cookie = name + "=" + value + expires + "; path=/";
+           },
+
+           readCookie: function (name) {
+               var nameEQ = name + "=";
+               var ca = document.cookie.split(';');
+               for (var i = 0; i < ca.length; i++) {
+                   var c = ca[i];
+                   while (c.charAt(0) === ' ') { c = c.substring(1, c.length); }
+                   if (c.indexOf(nameEQ) === 0) { return c.substring(nameEQ.length, c.length); }
+               }
+               return null;
+           },
+
+           destroyCookie: function (name) {
+               this.createCookie(name, "", -1);
+           }           
+        },
+        
+        URLInformation: {
+            
+            urlContains: function (pathArray, keepQueryString) {
+
+                var url, baseurl;
+
+                if (!keepQueryString || typeof keepQueryString === "undefined") {
+                    url = window.location.href;
+                    baseurl = url.split("?")[0];
+                } else {
+                    baseurl = window.location.href;
+                }
+
+                for (var i = 0; i < pathArray.length; i++) {
+                    if (baseurl.indexOf(pathArray[i]) === -1 && baseurl.indexOf(pathArray[i].toLowerCase()) === -1) {
+                        return false;
+                    }
+                }
+                return true; // did we reach the end? everything passed
+            },
+
+            hasParameter: function (name) {
+
+                // Get query string
+                var fullQString = window.location.search.substring(1);
+
+                if (fullQString.length > 0) {
+
+                    // Split string
+                    var paramArray = fullQString.split("&");
+
+                    //Loop through params, check if parameter exists.  
+                    for (var i = 0; i < paramArray.length; i++) {
+                        var currentParameter = paramArray[i].split("=");
+                        if (currentParameter[0] === name) { //Parameter already exists in current url
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            },
+            
+            getParameter: function (name) {
+                var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+                return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+            }            
+        }
+    },
+ 
     ConvertToSlug: function(text) {
 	return text.trim().toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
     },
